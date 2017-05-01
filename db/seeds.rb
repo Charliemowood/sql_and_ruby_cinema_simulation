410da1754033f28c70fe23ebe4928c7cfa1ce4a3
Charlie Wood <charliemowood@gmail.com>
2017-05-01 06:58:30 +0100

    Add save function to film class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-require_relative( '../models/customers' )
-require_relative( '../models/films' )
-require_relative( '../models/tickets' )
+require_relative( '../models/customer' )
+require_relative( '../models/film' )
+require_relative( '../models/ticket' )
 
 require( 'pry-byebug' )
 
@@ -8,6 +8,11 @@ customer1 = Customer.new({ 'name' => "Garry"})
 customer1.save()
 customer2 = Customer.new({ 'name' => "Harry"})
 customer2.save()
+
+film1 = Film.new({ 'title' => 'Star Wars', 'price' => 5})
+film1.save()
+film2 = Film.new({ 'title' => 'Too fast', 'price' => 4})
+film2.save()
 
 binding.pry
 nil
